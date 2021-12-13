2989f714c5baaba0fe6d3d9d0659f20979b6f9eb
Diquelou Morgane <diquelou.morgane@hotmail.fr>
2021-12-13 15:15:55 +0100

    Insert products function added

--- P5-Dev-Web-Kanap/front/js/script.js
+++ P5-Dev-Web-Kanap/front/js/script.js
@@ -1,16 +1,26 @@
+//Get all the products from the API//
 function showProducts() {
-    fetch("https://localhost:3000/api/products")
+    fetch("http://localhost:3000/api/products")
     .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }
+        return res.json();
     })
-    .then(function(value) {
-        console.log(value);
+    .then(function(data) {
+        showProducts(data);
     })
     .catch(function(err){
     });
 }
-const newElt = document.createElement("a");
-let elt = document.getElementById("items");
-elt.appendChild(newElt);
+//Insert products in the Index page//
+function showProducts(data) {
+    for (products of data) {
+    const items = document.getElementById()
+    items.innerHTML = "<a href="./product.html?id=42"><article>
+                        <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+                        <h3 class="productName">Kanap name1</h3>
+                        <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+                        </article>
+                        </a>"}
+    window.onload=function(){
+        showProducts();
+    }
+}}
