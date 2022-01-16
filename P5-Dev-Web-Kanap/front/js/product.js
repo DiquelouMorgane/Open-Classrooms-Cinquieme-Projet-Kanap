2c32efa98ddb6ea9d901fe3a9d3ec82de52279a3
Diquelou Morgane <diquelou.morgane@hotmail.fr>
2022-01-16 12:14:23 +0100

    Multiple articles function beggined

--- P5-Dev-Web-Kanap/front/js/product.js
+++ P5-Dev-Web-Kanap/front/js/product.js
@@ -1,3 +1,4 @@
+//--------------------------------------------------------Product Page Part-------------------------------------------------------------//
 const productId = getProductId();
 const productContent = getCardProducts(productId);
 var product;
@@ -31,11 +32,49 @@ function cardProducts(data) {
         );
       }
     }
-//stock the wanted product in the LocalStorage//
+//--------------------------------------------------------Basket Part-------------------------------------------------------------//
+//add the wanted product in an array, stock in the Local Storage//
+function newProductObject (arrayData) {
+    //Basket creation in the LocalStorage//
+    let cartStorage = JSON.parse(localStorage.getItem("cartStorage"));
+    if (cartStorage == null) {
+        cartStorage = [];
+    }
+    console.log(cartStorage);
+    //Create a new Product Card to save//
+    let productQuantity = document.querySelector('#quantity').value;
+    let productColor = document.querySelector('#colors').value;
+    let newProduct = {
+        id : `${arrayData._id}`,
+        quantity : productQuantity,
+        color : productColor,
+    };
+    localStorage.setItem('newProduct', JSON.stringify(newProduct));
+    //Add the wanted product for each product wanted//
+    let idValue = newProduct['id'];
+    let colorValue = newProduct['color'];
+    let quantityValue = newProduct['quantity'];
+    if (localStorage.cartStorage == null) {
+        cartStorage.push(newProduct);
+    } else if (localStorage.cartStorage.includes(idValue) && localStorage.cartStorage.includes(colorValue)) {
+        for (let i = 0; i < cartStorage.length; i++) {
+            if (cartStorage[i].id === idValue && cartStorage[i].color === colorValue) {
+                let index = cartStorage[i];
+                let newQuantityValue = Number(quantityValue) + Number(index.quantity);
+                index.quantity = newQuantityValue.toString();
+            }
+        }
+    } else {
+        cartStorage.push(newProduct);
+    }
+    //Save the new Basket if new quantity, color or product//
+    localStorage.setItem('cartStorage', JSON.stringify(cartStorage));
+}
+/*//save the wanted product in the LocalStorage//
 const addToCart = document.getElementById("addToCart");
 addToCart.addEventListener("click", function() {
     console.log(product)
     localStorage.setItem("cartStorage",JSON.stringify([{...product,color: document.getElementById('colors').value, quantity: document.getElementById('quantity').value}]));
     console.log(localStorage);
-    });
+    });*/
 };
